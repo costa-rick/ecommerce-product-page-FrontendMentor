7b97c81a3f6a465759bbff3e910cdc039c0f0a04
costa-rick <costa.rick.vi@gmail.com>
2023-02-25 12:57:43 -0300

    criando página do produto, script para contador e corrigindo folhas de estilo

---
+++ js/contador.js
@@ -0,0 +1,43 @@
+export default class Contador {
+	constructor(botaoIncremento, botaoDecremento, contador) {
+		this.botaoIncrementoEl = document.querySelector(botaoIncremento)
+		this.botaoDecrementoEl = document.querySelector(botaoDecremento)
+		this.contadorEl = document.querySelector(contador)
+
+		this.valorAtual = Number(this.contadorEl.innerText)
+
+		this.incrementar = this.incrementar.bind(this)
+		this.decrementar = this.decrementar.bind(this)
+	}
+
+	incrementar() {
+		this.valorAtual++
+		this.atribuirAoElemento(this.valorAtual)
+	}
+
+	decrementar() {
+		if (this.valorAtual < 1) {
+			return
+		}
+
+		this.valorAtual--
+		this.atribuirAoElemento(this.valorAtual)
+	}
+
+	atribuirAoElemento(valor) {
+		this.contadorEl.innerText = valor
+	}
+
+	adicionarEventos() {
+		this.botaoIncrementoEl.addEventListener('click', this.incrementar)
+		this.botaoDecrementoEl.addEventListener('click', this.decrementar)
+	}
+
+	iniciar() {
+		if (!this.botaoDecrementoEl || !this.botaoIncrementoEl || !this.contadorEl) {
+			throw new Error('Valores undefined não são aceitos, forneça argumentos válidos.')
+		}
+
+		this.adicionarEventos()
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import MenuMobile from './menu-mobile.js'
+import Contador from './contador.js'
 
 const menuMobile = new MenuMobile('.menu-icone', '.navegacao-icone-fechar', '.navegacao')
 menuMobile.iniciar()
+
+const contador = new Contador('#incremento', '#decremento', '#contador')
+contador.iniciar()
